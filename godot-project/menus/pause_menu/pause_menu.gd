e0cb770f136ad619544713264c4cf58ffb8c2348
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2019-04-28 13:24:00 +0100

    Added boilerplate from https://github.com/moisesjbc/godot-game-boilerplate

---
+++ godot-project/menus/pause_menu/pause_menu.gd
@@ -0,0 +1,21 @@
+extends CenterContainer
+
+
+func run():
+	pause_game()
+
+func pause_game():
+	get_tree().paused = true
+	visible = true
+
+func unpause_game():
+	get_tree().paused = false
+	visible = false
+
+func _on_return_button_pressed():
+	unpause_game()
+	get_tree().change_scene("res://menus/main_menu/main_menu.tscn")
+
+func _on_restart_button_pressed():
+	unpause_game()
+	get_tree().change_scene("res://main/main.tscn")
