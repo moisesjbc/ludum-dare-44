b4b8b19ee01941e7fcf27f66e2c882c15c5958b0
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2019-04-28 18:01:07 +0100

    Added disconnected fight screen

---
+++ godot-project/fight_screen/fight_screen.gd
@@ -0,0 +1,49 @@
+extends Node2D
+
+export (PackedScene) var quick_time_event_template = null
+var total_hits : int = 4
+var hit_time : float = 1
+var time_between_hits : float = 1
+var damage_per_hit : int = 20
+var money : int
+
+signal stopped
+
+func _ready():
+	start(400, 20, 3, 1.0)
+
+func start(money, damage_per_hit, total_hits, time_between_hits):
+	visible = true
+	self.money = money
+	self.damage_per_hit = damage_per_hit
+	self.total_hits = total_hits
+	self.time_between_hits = time_between_hits
+	$start_timer.start(1.0)
+	
+func stop():
+	emit_signal("stopped")
+	print("stopped")
+
+func _on_start_timer_timeout():
+	var quick_time_event = quick_time_event_template.instance()
+	quick_time_event.position.x = randi() % int($bottom_right_corner.position.x - $upper_left_corner.position.x) + $upper_left_corner.position.x
+	quick_time_event.position.y = randi() % int($bottom_right_corner.position.y - $upper_left_corner.position.y) + $upper_left_corner.position.y
+	add_child(quick_time_event)
+	
+	quick_time_event.connect("quick_time_event_failed", self, "_on_quick_time_event_failed")
+	quick_time_event.connect("quick_time_event_succeeded", self, "_on_quick_time_event_succeeded")
+
+func finish_hit():
+	total_hits -= 1
+	print('total_hits')
+	print(total_hits)
+	if total_hits > 0:
+		$start_timer.start(self.time_between_hits)
+	else:
+		stop()
+
+func _on_quick_time_event_failed():
+	finish_hit()
+	
+func _on_quick_time_event_succeeded():
+	finish_hit()
---
+++ godot-project/hit_quick_time_event/hit_quick_time_event.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+signal quick_time_event_failed
+signal quick_time_event_succeeded
+
+func _on_hit_quick_time_event_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		emit_signal('quick_time_event_succeeded')
+		queue_free()
+
+func _on_Timer_timeout():
+	emit_signal('quick_time_event_failed')
+	queue_free()
