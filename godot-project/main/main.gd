260e85f647317962413ffb6ca26e7275de6d3273
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2019-05-01 11:37:17 +0100

    Fixed game over screen not centered

--- godot-project/main/main.gd
+++ godot-project/main/main.gd
@@ -4,21 +4,21 @@ export (NodePath) var currency_change_screen_path = null
 
 func _process(delta):
 	if Input.is_action_pressed("ui_pause"):
-		$pause_menu.pause_game()
+		$gui/pause_menu.pause_game()
 
 
 func _on_change_machine_currency_change_started(person, change_machine):
-	$gui.pause_timer()
-	$change_screen.start(person, change_machine)
+	$gui/gui.pause_timer()
+	$gui/change_screen.start(person, change_machine)
 
 
 func _on_gui_time_finished():
 	var current_money = $change_machine.money
 	if $change_machine.person_charging_currency:
 		 current_money -= $change_machine.person_charging_currency.money
-	$change_screen.visible = false
-	$game_over_menu.run(current_money - $change_machine.start_money)
+	$gui/change_screen.visible = false
+	$gui/game_over_menu.run(current_money - $change_machine.start_money)
 
 
 func _on_change_machine_currency_change_finished():
-	$gui.resume_timer()
+	$gui/gui.resume_timer()
